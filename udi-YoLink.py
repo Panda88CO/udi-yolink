7615974fae3a3312de48d79c86d0eea5efbeafba
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2022-10-10 12:51:11 -0700

    Update udi-YoLink.py

--- udi-YoLink.py
+++ udi-YoLink.py
@@ -464,7 +464,7 @@ class YoLinkSetup (udi_interface.Node):
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([])
-        polyglot.start('0.5.9')
+        polyglot.start('0.5.10')
         YoLinkSetup(polyglot, 'setup', 'setup', 'YoLinkSetup')
 
         # Just sit and wait for events
