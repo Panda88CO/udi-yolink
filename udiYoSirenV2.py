b34588625d75d319d043b829e6e6b9fa8a418741
Panda88CO <christian.olgaard@gmail.com>
2023-09-20 17:42:23 -0700

    Update udiYoSirenV2.py

--- udiYoSirenV2.py
+++ udiYoSirenV2.py
@@ -88,8 +88,6 @@ class udiYoSiren(udi_interface.Node):
         time.sleep(4)
         self.yoSiren.initNode()
         time.sleep(2)
-        #self.node.setDriver('ST', 1, True, True)
-        self.yoSiren.delayTimerCallback (self.updateDelayCountdown, self.timer_update)
         self.node_ready = True
 
     def stop (self):
@@ -114,16 +112,19 @@ class udiYoSiren(udi_interface.Node):
         if self.node is not None:
             state =  self.yoSiren.getState()
             if self.yoSiren.online:
-                if state.upper() == 'OPEN':
+                if state.upper() == 'NORMAL':
                     self.valveState = 1
                     self.node.setDriver('GV0', self.valveState , True, True)
                     if self.last_state != state:
-                        self.node.reportCmd('DON')
-                elif state.upper() == 'CLOSED':
+                        self.node.reportCmd('DOF')
+                elif state.upper() == 'ALERT':
                     self.valveState = 0
                     self.node.setDriver('GV0', self.valveState , True, True)
                     if self.last_state != state:
-                        self.node.reportCmd('DOF')
+                        self.node.reportCmd('DON')
+                elif state.upper() == 'OFF':
+                    self.valveState = 0
+                    self.node.setDriver('GV0', self.valveState , True, True)
                 else:
                     self.node.setDriver('GV0', 99, True, True)
                     
@@ -150,68 +151,34 @@ class udiYoSiren(udi_interface.Node):
       
 
 
-    def updateDelayCountdown( self, timeRemaining):
 
-        logging.debug('Siren updateDelayCountDown:  delays {}'.format(timeRemaining))
-        max_delay = 0
-        for delayInfo in range(0, len(timeRemaining)):
-            if 'ch' in timeRemaining[delayInfo]:
-                if timeRemaining[delayInfo]['ch'] == 1:
-                    if 'on' in timeRemaining[delayInfo]:
-                        self.node.setDriver('GV1', timeRemaining[delayInfo]['on'], True, False)
-                        if max_delay < timeRemaining[delayInfo]['on']:
-                            max_delay = timeRemaining[delayInfo]['on']
-                    if 'off' in timeRemaining[delayInfo]:
-                        self.node.setDriver('GV2', timeRemaining[delayInfo]['off'], True, False)
-                        if max_delay < timeRemaining[delayInfo]['off']:
-                            max_delay = timeRemaining[delayInfo]['off']
-                    self.node.setDriver('GV0', self.valveState , True, True)
-        self.timer_expires = time.time()+max_delay
-  
     def switchControl(self, command):
         logging.info('Siren switchControl')
         state = int(command.get('value'))
         if state == 1:
-            self.yoSiren.setState('open')
-            self.valveState = 1
+            self.yoSiren.setState('on')
+            self.valveState = True
             self.node.setDriver('GV0',self.valveState  , True, True)
    
             #self.node.reportCmd('DON')
         else:
-            self.yoSiren.setState('closed')
-            self.valveState  = 0
+            self.yoSiren.setState('off')
+            self.valveState  = False
             self.node.setDriver('GV0',self.valveState , True, True)
             #self.node.reportCmd('DOF')
 
     def set_open(self, command = None):
-        logging.info('Siren - set_open')
-        self.yoSiren.setState('open')
-        self.valveState  = 1
+        logging.info('Siren - set_alert')
+        self.yoSiren.setState('on')
+        self.valveState  = True
         self.node.setDriver('GV0',self.valveState  , True, True)
 
         #self.node.reportCmd('DON')
 
     def set_close(self, command = None):
-        logging.info('Siren - set_close')
-        self.yoSiren.setState('closed')
-        self.valveState  = 0
-        self.node.setDriver('GV0',self.valveState  , True, True)
-
-        #self.node.reportCmd('DOF')
-
-
-    def setOnDelay(self, command ):
-        logging.info('setOnDelay')
-        delay =int(command.get('value'))
-        self.yoSiren.setOnDelay(delay)
-        self.node.setDriver('GV1', delay*60, True, True)
-        self.node.setDriver('GV0',self.valveState  , True, True)
-
-    def setOffDelay(self, command):
-        logging.info('setOnDelay Executed')
-        delay =int(command.get('value'))
-        self.yoSiren.setOffDelay(delay)
-        self.node.setDriver('GV2', delay*60, True, True)
+        logging.info('Siren - set_normal')
+        self.yoSiren.setState('off')
+        self.valveState  = False
         self.node.setDriver('GV0',self.valveState  , True, True)
 
 
@@ -227,9 +194,8 @@ class udiYoSiren(udi_interface.Node):
                 'QUERY' : update,
                 'DON'   : set_open,
                 'DOF'   : set_close,
-                'MANCTRL': switchControl, 
-                'ONDELAY' : setOnDelay,
-                'OFFDELAY' : setOffDelay 
+                'ALARMCTRL': switchControl, 
+
                 }
 
 
