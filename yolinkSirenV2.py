0f33bca1afb84db2387561e3c7ec0f6fb76a1b0e
Panda88CO <christian.olgaard@gmail.com>
2023-09-25 16:53:25 -0700

    Update yolinkSirenV2.py

--- yolinkSirenV2.py
+++ yolinkSirenV2.py
@@ -43,9 +43,9 @@ class YoLinkSir(YoLinkMQTTDevice):
         #yolink.online = yolink.getOnlineStatus()
         if yolink.online:
             if state == 'on':
-                state = True
+                state = 'on'
             if state == 'off':
-                state = False
+                state = 'off'
             else:
                 logging.error('Unknows state passed')
                 return(False)
