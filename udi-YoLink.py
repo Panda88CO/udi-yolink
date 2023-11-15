6c209506698bf8a0c41645e1e897488f87984b48
Panda88CO <christian.olgaard@gmail.com>
2023-11-15 10:50:46 -0800

    Update udi-YoLink.py

--- udi-YoLink.py
+++ udi-YoLink.py
@@ -545,9 +545,9 @@ class YoLinkSetup (udi_interface.Node):
         for nde in range(0, len(self.nodes_in_db)):
             node = self.nodes_in_db[nde]
             logging.debug('Scanning db for extra nodes : {}'.format(node))
-            if node['address']  not in self.assigned_addresses:
+            if node['address']  not in self.assigned_addresses and node['isPrimary'] == 1: # do not remove sub-nodes
                 logging.debug('Removing node : {} {}'.format(node['name'], node))
-                self.poly.delNode(node)
+                self.poly.delNode(node['address'])
                 
         # checking params for erassed nodes
         self.poly.updateProfile()
