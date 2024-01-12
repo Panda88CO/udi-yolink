81f9e5e450f70ea8680fead7624d464a048d2a20
Panda88CO <65139210+Panda88CO@users.noreply.github.com>
2024-01-11 22:22:20 -0800

    Update yolink_mqtt_classV3.py

--- yolink_mqtt_classV3.py
+++ yolink_mqtt_classV3.py
@@ -1018,10 +1018,10 @@ class YoLinkMQTTDevice(object):
                     yolink.nbrUsb = data['data']['delays'][0]['ch']
                     yolink.nbrPorts = yolink.nbrOutlets + yolink.nbrUsb
             if 'method' in data:
-                if 'data' in data:
-                    yolink.dataAPI[yolink.dData] = data['data']
+                if 'data' in data:  #new
+                    yolink.dataAPI[yolink.dData] = data['data'] #new
                 if yolink.dState in data[yolink.dData]:
-                    yolink.dataAPI[yolink.dData][yolink.dState] = {}
+                    yolink.dataAPI[yolink.dData][yolink.dState] = {} #new
                     #if 'reportAt' in data[yolink.dData] or 'stateChangedAt' in data[yolink.dData]:
                     #    reportAt = datetime.strptime(data[yolink.dData]['reportAt'], '%Y-%m-%dT%H:%M:%S.%fZ')
                     #    yolink.dataAPI['lastStateTime'] = (reportAt.timestamp() -  yolink.timezoneOffsetSec)*1000
