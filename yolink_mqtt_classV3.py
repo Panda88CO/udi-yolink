ff1bf73b49b0006a8e5cb1ce750195b4b38ee4ab
Panda88CO <christian.olgaard@gmail.com>
2024-02-28 11:56:55 -0800

    Update yolink_mqtt_classV3.py

--- yolink_mqtt_classV3.py
+++ yolink_mqtt_classV3.py
@@ -1119,9 +1119,9 @@ class YoLinkMQTTDevice(object):
                                     yolink.nbrUsb = 0
                                     yolink.nbrPorts = yolink.nbrOutlets + yolink.nbrUsb
                                 else:
-                                    #logging.debug('addind 1 {} {}:'.format(key,data[yolink.dData] ))
-                                    #logging.debug('addind 2 {} {}:'.format(key, yolink.dataAPI[yolink.dData][yolink.dState]))  
-                                    #logging.debug('addind 3 {} {}:'.format(key, data[yolink.dData][key]))
+                                    logging.debug('adding 1 {} {}:'.format(key,data[yolink.dData] ))
+                                    logging.debug('adding 2 {} {}:'.format(key, yolink.dataAPI[yolink.dData][yolink.dState]))  
+                                    logging.debug('adding 3 {} {}:'.format(key, data[yolink.dData][key]))
                                     if yolink.dState not in yolink.dataAPI[yolink.dData]:
                                         yolink.dataAPI[yolink.dData][yolink.dState] = {}
                                         logging.debug('dState added')
