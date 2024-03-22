1c372e3ece061b8663090c4e31bdc8c8fe55d89c
Panda88CO <christian.olgaard@gmail.com>
2024-03-22 16:03:01 -0700

    Update udi-YoLink.py

--- udi-YoLink.py
+++ udi-YoLink.py
@@ -675,8 +675,8 @@ class YoLinkSetup (udi_interface.Node):
                             logging.debug('shortpoll {}'.format(nde))
                             # no API calls so no need to spread out 
                             #time.sleep(4)  # need to limit calls to 100 per  5 min - using 4 to allow other calls
-            else:
-                #self.node.setDriver('ST', 0, True, True)
+            #else:
+            #    self.node.setDriver('ST', 0, True, True)
                 
 
 
