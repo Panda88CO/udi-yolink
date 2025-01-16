9f07293271a839fe1bbad89cef8dbbfe96863578
Panda88CO <christian.olgaard@gmail.com>
2025-01-15 17:34:29 -0800

    1.3.20

--- udi-YoLink.py
+++ udi-YoLink.py
@@ -46,7 +46,7 @@ except ImportError:
 
 
 
-version = '1.3.18'
+version = '1.3.20'
 
 class YoLinkSetup (udi_interface.Node):
     from  udiYolinkLib import my_setDriver,node_queue, wait_for_node_done
