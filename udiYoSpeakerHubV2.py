066ba3013e92fba9c16a8e5da1a8c2868a7e7955
Christian Olgaard <colgaard@litepoint.com>
2025-07-08 19:52:11 +0200

    test

--- udiYoSpeakerHubV2.py
+++ udiYoSpeakerHubV2.py
@@ -24,12 +24,12 @@ class udiYoSpeakerHub(udi_interface.Node):
     from  udiYolinkLib import my_setDriver
     id = 'yospeakerh'
     drivers = [
-            {'driver': 'GV0', 'value': 5, 'uom': 107}, 
+            {'driver': 'GV0', 'value': 5, 'uom': 56}, 
             {'driver': 'GV1', 'value': 0, 'uom': 25}, 
             {'driver': 'GV2', 'value': 0, 'uom': 25}, 
             {'driver': 'GV3', 'value': 0, 'uom': 25}, 
             {'driver': 'GV4', 'value': 0, 'uom': 25}, 
-            {'driver': 'GV5', 'value': 0, 'uom': 107},        
+            {'driver': 'GV5', 'value': 0, 'uom': 56},        
             {'driver': 'ST', 'value': 0, 'uom': 25},
             {'driver': 'ST', 'value': 0, 'uom': 25},
             {'driver': 'GV20', 'value': 99, 'uom': 25}, 
