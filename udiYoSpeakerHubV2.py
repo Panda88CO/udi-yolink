f88cf12e98871cfe281ece3b4c2c0fb52e671372
Christian Olgaard <colgaard@litepoint.com>
2025-07-09 23:07:15 +0200

    Update udiYoSpeakerHubV2.py

--- udiYoSpeakerHubV2.py
+++ udiYoSpeakerHubV2.py
@@ -247,7 +247,6 @@ class udiYoSpeakerHub(udi_interface.Node):
             volume =  int(query.get("volume.uom56"))
             self.my_setDriver('GV0',volume )
             tone_nbr =  int(query.get("tone.uom25"))
-            
             tone = tones[tone_nbr]
             self.my_setDriver('GV3', tone_nbr )
             logging.debug(f'tone: {tone}')
